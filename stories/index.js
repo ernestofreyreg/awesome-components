87d6df8480c400999a8f71a75ab01200638775cd
Ernesto Freyre <ernesto@bettercompany.co>
2017-08-04 01:39:10 -0700

    Added storybook

---
+++ stories/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import { storiesOf } from '@storybook/react'
+import { action } from '@storybook/addon-actions'
+import { linkTo } from '@storybook/addon-links'
+import {Button} from '../src/index'
+
+
+storiesOf('Button', module)
+  .add('simple text', () => <Button title='Hello Button' onClick={action('clicked Hello Button')} />)
+  .add('with some emoji', () => <Button title='😀 😎 👍 💯' onClick={action('clicked Emojis')} />)
